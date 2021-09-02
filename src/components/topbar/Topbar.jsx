49ca438d4b8abfdf33a57204a99c1d1d10080b66
Atishay <atishay005@gmail.com>
2021-09-02 12:26:38 +0530

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import "./topbar.scss"
+import { Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">portfolio.</a>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>atishay005@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
